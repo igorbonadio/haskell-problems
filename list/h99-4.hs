ed7b5aa17fa9fdd0f6085fd85f0e9fa2960cf7d8
Igor Bonadio <igorbonadio@gmail.com>
2014-09-09 14:56:04 -0300

    Adds tail recursive solution of H99-4

--- list/h99-4.hs
+++ list/h99-4.hs
@@ -1,3 +1,12 @@
+-- recursive solution
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (x:xs) = myLength xs + 1
+
+-- tails recursive
+myLength' :: [a] -> Int
+myLength' xs = myLength_acc xs 0
+  where
+    myLength_acc [] n = n
+    myLength_acc (x:xs) n = myLength_acc xs (n + 1)
+
