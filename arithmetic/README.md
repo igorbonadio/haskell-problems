ab872f77e8ac7a35b6c407edd90266a6b9d03ce3
Igor Bonadio <igorbonadio@gmail.com>
2014-09-12 18:45:03 -0300

    Adds H99-37

--- arithmetic/README.md
+++ arithmetic/README.md
@@ -79,3 +79,25 @@ _Example_:
 ```haskell
 primeFactorsMult 315 -- [(3,2),(5,1),(7,1)]
 ```
+
+## H99-37
+
+Calculate Euler's totient function phi(m) (improved).
+
+See problem 34 for the definition of Euler's totient function. If the list of the prime factors of a number m is known in the form of problem 36 then the function phi(m) can be efficiently calculated as follows: Let ((p1 m1) (p2 m2) (p3 m3) ...) be the list of prime factors (and their multiplicities) of a given number m. Then phi(m) can be calculated with the following formula:
+
+```
+phi(m) = (p1 - 1) * p1 ** (m1 - 1) *
+         (p2 - 1) * p2 ** (m2 - 1) *
+         (p3 - 1) * p3 ** (m3 - 1) * ...
+```
+
+Note that a ** b stands for the b'th power of a.
+
+_Level_: 1
+
+_Example_:
+
+```haskell
+totient 10 -- 4
+```
---
+++ arithmetic/h99-37.hs
@@ -0,0 +1,30 @@
+isPrime :: Int -> Bool
+isPrime x = isPrimeAux x (floor . sqrt . fromIntegral $ x)
+  where
+    isPrimeAux x 1 = True
+    isPrimeAux x y
+      | x `mod` y == 0 = False
+      | otherwise = isPrimeAux x (y-1)
+
+
+primeFactors :: Int -> [Int]
+primeFactors n = [x | x <- [2..n]
+                    , n `mod` x == 0
+                    , isPrime x]
+
+factorMultiplicity :: Int -> Int -> Int
+factorMultiplicity 1 _ = 0
+factorMultiplicity n x
+  | n `mod` x == 0 = 1 + factorMultiplicity (n `div` x) x
+  | otherwise = 0
+
+primeFactorsMult :: Int -> [(Int, Int)]
+primeFactorsMult n = map multiplicity (primeFactors n)
+  where
+    multiplicity x = (x, factorMultiplicity n x)
+
+totient :: Int -> Int
+totient n = totientAux (primeFactorsMult n)
+  where
+    totientAux [] = 1
+    totientAux ((p, m):xs) = (p - 1) * (p ^ (m - 1)) * totientAux xs
